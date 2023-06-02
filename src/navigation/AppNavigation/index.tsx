4615155eedbc94955e2326aa7590be95820a4607
Hung Nguyen <hungnguyen99.nvh@gmail.com>
2023-06-03 01:18:00 +0700

    add splash screen

--- src/navigation/AppNavigation/index.tsx
+++ src/navigation/AppNavigation/index.tsx
@@ -1,6 +1,8 @@
 import { navLinking } from '@/constants';
+import { delay } from '@/utils';
 import { NavigationContainer } from '@react-navigation/native';
 import React, { Fragment } from 'react';
+import SplashScreen from 'react-native-splash-screen';
 import AuthStack from '../AuthStack';
 import MainStack from '../MainStack';
 import { setNavigator } from '../service';
@@ -9,7 +11,8 @@ const isLogin = false;
 
 const AppNavigation: React.FC = () => {
   const bootstrap = async () => {
-    //
+    await delay(500);
+    SplashScreen.hide();
   };
 
   return (
